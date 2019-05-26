4b4e4456dfff18b0f72c749ee6e5d92704b06c50
Vladimir Tsyuman <Vladimir.Tsyuman@acronis.com>
2019-05-26 23:26:11 +0300

    commit

--- runner.py
+++ runner.py
@@ -2,19 +2,19 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.webdriver import WebDriver
 
 from helpers import get_info_by_elements
-from pages import StartPage, RECOMMENDED
+from pages import StartPage, RECOMMENDED, RECOMMENDED, DATE, NAME, PRICE_MIN, PRICE_MAX, BRAND
 
 
-def app():
+def app(min_price, max_price, sorting_type, search_string):
     chrome_options = Options()
     chrome_options.add_argument("--disable-notifications")
     chrome_options.add_argument("--start-maximized")
     browser = WebDriver(chrome_options=chrome_options)
     browser.get("https://iledebeaute.ru/")
     start = StartPage(browser)
-    products_page = start.search_by_name("esc")
-    products_page.pick_sorting(RECOMMENDED)
-    products_page.pick_price(123, 8000)
+    products_page = start.search_by_name(search_string)
+    products_page.pick_sorting(sorting_type)
+    products_page.pick_price(min_price, max_price)
     products_list = products_page.get_all_products()
     print(get_info_by_elements(products_list))
     browser.quit()
